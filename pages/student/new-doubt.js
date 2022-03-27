ff4cfa6d1ad2b570ec03f8a0787b96370c556325
KashishJain <55389276+Kashishjain04@users.noreply.github.com>
2022-03-28 00:47:43 +0530

    new-doubt page

--- pages/student/new-doubt.js
+++ pages/student/new-doubt.js
@@ -26,61 +26,53 @@ const topics = [
 ];
 
 function add() {
-	
 	return (
 		<div className="addDoubt">
 			<Navbar />
 
 			<div className="addDoubt_body">
+				<h1 className="addDoubt_mainHead">Add Doubt</h1>
 
-			<h1>Add Doubt</h1>
-			
-
-				<div className="addDoubt_body_form">
-
-
-					
-					
-
-					<div className="addDoubt_body_form_language">
-						<label htmlFor="">Language</label>
-						<div className="addDoubt_languages">
-						{topics.map( (t,i)=>( <p key={i}>{t}</p> ) )}
+				<div className="addDoubt_form">
+					<div className="addDoubt_language">
+						<label htmlFor="" className="addDoubt_heading">
+							Language
+						</label>
+						<div className="list">
+							{topics.map((t, i) => (
+								<p key={i}>{t}</p>
+							))}
 						</div>
-						
 					</div>
-					<div className="addDoubt_body_form_difficulty">
-						<label htmlFor="">Difficulty</label>
-						<div className="addDoubt_difficulty">
+					<div className="addDoubt_difficulty">
+						<label htmlFor="" className="addDoubt_heading">
+							Difficulty
+						</label>
+						<div className="list">
 							<p>Easy</p>
 							<p>Medium</p>
 							<p>Hard</p>
 						</div>
 					</div>
 
-
-                    <div>
-					<label htmlFor="">Screenshot</label>
-					<input type="file" />
+					<div>
+						<label htmlFor="" className="addDoubt_heading">Screenshot</label>
+						<input type="file" />
 					</div>
 
-
-                    <div>
-					<label htmlFor="">Short Description</label>
-					<textarea name="" id="" cols="30" rows="2"></textarea>
+					<div>
+						<label htmlFor="" className="addDoubt_heading">Short Description</label>
+						<textarea name="" id="" cols="30" rows="2"></textarea>
 					</div>
 
-                    <div>
-					<label htmlFor="">Long Description</label>
-					<textarea name="" id="" cols="30" rows="3"></textarea>
+					<div>
+						<label htmlFor="" className="addDoubt_heading">Long Description</label>
+						<textarea name="" id="" cols="30" rows="3"></textarea>
 					</div>
 
-					<button>Add</button>
-
+					<div className="addDoubt_btnParent"><button className="addDoubt_btn">Add</button></div>
 				</div>
-
 			</div>
-			
 		</div>
 	);
 }
