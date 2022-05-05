2f3f85b7c41b915008e2924eaedd63a35c2f8356
KashishJain <55389276+Kashishjain04@users.noreply.github.com>
2022-05-05 11:56:20 +0530

    adjust tailwind preflight

--- components/debugger/DoubtCard.js
+++ components/debugger/DoubtCard.js
@@ -1,41 +1,40 @@
-import React from 'react';
-import ReactLoading from 'react-loading';
+import React from "react";
+import ReactLoading from "react-loading";
 import Link from "next/link";
-import Modal from 'react-modal';
-import  {useState} from 'react';
-import DoubtModal from "./DoubtModal.js"
+import Modal from "react-modal";
+import { useState } from "react";
+import DoubtModal from "./DoubtModal.js";
 Modal.setAppElement("#root");
 
-function DoubtCard({status}) {
+function DoubtCard({ status }) {
+	const [modal, setModal] = useState(false);
 
-	const[modal,setModal]=useState(false);
-
-	const OnModal=()=>{
-
+	const OnModal = () => {
 		setModal(true);
+	};
 
-	}
-
-	const offModal=()=>{
+	const offModal = () => {
 		setModal(false);
-	}
-
+	};
 
 	return (
 		<div className="doubtCard">
 			<div className="doubtCard_navbar">
 				{/* <p>Active</p> */}
-				<ReactLoading type={status=='active'?"blank":"bars"} color="gray" height="9%" width="9%" />
+				<ReactLoading
+					type={status == "active" ? "blank" : "bars"}
+					color="gray"
+					height="9%"
+					width="9%"
+				/>
 				<h2>Java</h2>
 				<p>12-3-2022</p>
-				
 			</div>
 
 			<div className="doubtCard_body">
 				<div>
 					<h5 className="heading">Amount</h5>
 					<p>₹40</p>
-					
 				</div>
 				<div>
 					<h5 className="heading">Description</h5>
@@ -47,63 +46,45 @@ function DoubtCard({status}) {
 				</div>
 			</div>
 			<div className="doubtCard_footer">
-				<button onClick={ OnModal } className="doubtCard_view">View</button>
+				<button onClick={OnModal} className="doubtCard_view">
+					View
+				</button>
 
-                { status=='active' && 
-				<button     className="doubtCard_request">  Request</button>
-	             }
-			  
-			  
-				
+				{status == "active" && <button className="doubtCard_request"> Request</button>}
 			</div>
 
-			<Modal isOpen={modal} onRequestClose={ ()=>{setModal(false)} } 
-
-			style={
-				{
-					overlay:{
-
-
-						backgroundColor:'rgba(105,105,105,0.3)',
-						zIndex:'2',
-						
+			<Modal
+				isOpen={modal}
+				onRequestClose={() => {
+					setModal(false);
+				}}
+				style={{
+					overlay: {
+						backgroundColor: "rgba(105,105,105,0.3)",
+						zIndex: "2",
 					},
-					content:{
+					content: {
+						padding: "0",
+						marginLeft: "27vw",
+						marginTop: "10vh",
+						marginBottom: "10vh",
 
-						
+						border: "1px solid black",
+						background: "#fff",
 
-						padding:"0",
-						marginLeft:"27vw",
-						marginTop:"10vh",
-						marginBottom:"10vh",
-                         
-                         border: '1px solid black',
-                         background: '#fff',
-                        
-                         WebkitOverflowScrolling: 'touch',
-                         borderRadius: '4px',
+						WebkitOverflowScrolling: "touch",
+						borderRadius: "4px",
 						//  overflowY:"auto",
 
-						 scrollbarWidth:"0",
-						 
-                        
-						 
-						width:"50%"
-                         
-					}
-				}
-			}
-			 >
+						scrollbarWidth: "0",
 
-				 <DoubtModal offModal={offModal} />
-
-				
+						width: "50%",
+					},
+				}}
+			>
+				<DoubtModal offModal={offModal} />
 			</Modal>
-
-
-
 		</div>
-
 	);
 }
 
