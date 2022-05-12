fad09d463dbaa24846394f1147660560c2f99048
KashishJain <55389276+Kashishjain04@users.noreply.github.com>
2022-05-12 20:09:57 +0530

    student doubtcard dropdown always show

--- components/student/DoubtCard.js
+++ components/student/DoubtCard.js
@@ -4,6 +4,7 @@ function StudentDoubtCard() {
 
 	return (
 		<div className="studentDoubtCard">
+			<p className="studentDoubtCard_date">{new Date().toLocaleString("in")}</p>
 			<h4 className="studentDoubtCard_topic">Java</h4>
 			<ul className="studentDoubtCard_dropdown">
 				<Link href="/student/doubt/123">
@@ -33,8 +34,6 @@ function StudentDoubtCard() {
 				Having Trouble in question &quot;geek collects the balls&quot; from GFG . I think my approach
 				is correct but not getting correct output.Want help to know whats wrong in my approach.
 			</p>
-			<br />
-			<p className="studentDoubtCard_date">{new Date().toLocaleString("in")}</p>
 		</div>
 	);
 }
